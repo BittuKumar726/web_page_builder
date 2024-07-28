a2cc2e137ae165a1fd1d66d3aae1de07a212b015
AdityaRaj140 <17gietucse140@gmail.com>
2024-07-28 21:16:59 +0530

    Updated

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -3,10 +3,15 @@ import { useDrag } from "react-dnd";
 
 interface DraggableItemProps {
   type: string;
+  bgColor: string;
   children: React.ReactNode;
 }
 
-const DraggableItem: React.FC<DraggableItemProps> = ({ type, children }) => {
+const DraggableItem: React.FC<DraggableItemProps> = ({
+  type,
+  bgColor,
+  children,
+}) => {
   // Set up drag functionality using react-dnd
   const [{ isDragging }, drag] = useDrag(() => ({
     type, // Type of draggable item
@@ -22,6 +27,7 @@ const DraggableItem: React.FC<DraggableItemProps> = ({ type, children }) => {
       className={`p-2 m-2 border rounded border-gray-400 cursor-pointer ${
         isDragging ? "opacity-50" : "opacity-100" // Adjust opacity based on dragging state
       }`}
+      style={{ backgroundColor: bgColor }}
     >
       {children}
     </div>
@@ -30,10 +36,22 @@ const DraggableItem: React.FC<DraggableItemProps> = ({ type, children }) => {
 
 const Toolbar: React.FC = () => {
   return (
-    <div className="flex justify-center p-2 bg-gray-100 border-b border-gray-300">
-      <DraggableItem type="text">Text</DraggableItem>
-      <DraggableItem type="image">Image</DraggableItem>
-      <DraggableItem type="button">Button</DraggableItem>
+    <div className="flex justify-between p-2 bg-gray-100 border-b border-gray-300 items-center">
+      <div className="w-[35%] p-2">
+        <span className="font-bold">Web page builder</span>
+      </div>
+      <div className="flex w-[50%]">
+        <DraggableItem type="text" bgColor="white">
+          Text
+        </DraggableItem>
+        <DraggableItem type="image" bgColor="#c6de75">
+          Image
+        </DraggableItem>
+        <DraggableItem type="button" bgColor="#3b82f6">
+          Button
+        </DraggableItem>
+      </div>
+      <div></div>
     </div>
   );
 };
